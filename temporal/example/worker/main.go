ce4d46fc8477bd5d28fa91718d84def033940947
Lovro Mažgon <lovro.mazgon@gmail.com>
2023-04-10 20:40:46 +0200

    check error

--- temporal/example/worker/main.go
+++ temporal/example/worker/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/lovromazgon/fsm/example"
 	"github.com/lovromazgon/fsm/temporal"
 )
 
 func main() {
-	temporal.RunWorker[example.FooState, example.FooObservation, *example.FooInstance](example.FooDef{})
+	err := temporal.RunWorker[example.FooState, example.FooObservation, *example.FooInstance](example.FooDef{})
+	if err != nil {
+		log.Fatalln("Failed to run worker", err)
+	}
 }
