a2c47a902c560436063ed6d282a4a850f26fb170
Lovro Mažgon <lovro.mazgon@gmail.com>
2023-04-11 20:41:43 +0200

    visualize fsm graph

---
+++ cmd/fsm-to-graph/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/goccy/go-graphviz"
+	"github.com/lovromazgon/fsm/example"
+	"github.com/lovromazgon/fsm/graph"
+)
+
+func main() {
+	// TODO make this utility work on any state machine (maybe use the same
+	//  approach as gomock reflect mode https://github.com/golang/mock/blob/main/mockgen/reflect.go#L45)
+	fooGraph, err := graph.DefToGraph[example.FooState, example.FooObservation, *example.FooInstance](example.FooDef{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	g := graphviz.New()
+
+	if err := g.RenderFilename(fooGraph, graphviz.PNG, "./foograph.png"); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ graph/graph.go
@@ -0,0 +1,38 @@
+package graph
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/goccy/go-graphviz"
+	"github.com/goccy/go-graphviz/cgraph"
+	"github.com/lovromazgon/fsm"
+)
+
+func DefToGraph[S fsm.State, O any, I fsm.Instance[S, O]](def fsm.Definition[S, O, I]) (*cgraph.Graph, error) {
+	g := graphviz.New()
+	graph, err := g.Graph(graphviz.Directed)
+	if err != nil {
+		return nil, fmt.Errorf("could not create graph: %w", err)
+	}
+
+	nodes := make(map[S]*cgraph.Node)
+	for _, state := range def.States() {
+		n, err := graph.CreateNode(string(state))
+		if err != nil {
+			return nil, fmt.Errorf("could not create node %v: %w", state, err)
+		}
+		nodes[state] = n
+	}
+
+	for i, transition := range def.Transitions() {
+		name := strconv.Itoa(i) // TODO display nicer name
+		e, err := graph.CreateEdge(name, nodes[transition.From], nodes[transition.To])
+		if err != nil {
+			return nil, fmt.Errorf("could not create edge %v: %w", name, err)
+		}
+		e.SetLabel(name)
+	}
+
+	return graph, nil
+}
